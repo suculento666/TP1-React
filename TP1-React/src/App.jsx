87e901f0229d04b608283e945f56a4c29a50b7ff
FIDEL GOLDSMAN <49002222@ort>
2026-03-31 10:34:27 -0300

    Finished

--- TP1-React/src/App.jsx
+++ TP1-React/src/App.jsx
@@ -1,28 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+
 import Header from './components/Header'
 import Form from './components/Form'
 import Listado from './components/Listado'
 function App() {
-  const [count, setCount] = useState(0)
-
-
-  return (
+ return (
     <>
       <Header />
-      <main className="app-container">
-        <section className="app-grid">
-          <div className="left">
-            <Form />
-          </div>
-          <div className="right">
-            <Listado />
-          </div>
-        </section>
-      </main>
+      <div className='container'>
+        <div class = "row">
+          <div className='one-half column'> <Form /></div>
+          <div className='one-half column'> <Listado /></div>
+        </div>
+        
+      </div>
+      
+      
     </>
   )
 }
